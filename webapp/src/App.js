a0462f28c65e3fb6ab4c1fe03895e6e03055c286
Mauro Rocchi <maurocchi@gmail.com>
2019-10-27 18:43:09 +0100

    configure theme

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,16 +1,27 @@
-import React, { Fragment } from "react";
+import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
+import indigo from "@material-ui/core/colors/indigo";
 import { Center } from "./Layout";
 import { Welcome } from "./Welcome";
 
+const theme = createMuiTheme({
+  palette: {
+    background: {
+      default: indigo[400]
+    }
+  }
+});
+
 function App() {
   return (
-    <Fragment>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Center>
         <Welcome />
       </Center>
-    </Fragment>
+    </ThemeProvider>
   );
 }
 
