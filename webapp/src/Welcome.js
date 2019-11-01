be5399e8635a11c17acf6bb00c267d82534b87b2
Mauro Rocchi <maurocchi@gmail.com>
2019-11-01 11:08:57 +0100

    refactor welcome panel using loading button

--- webapp/src/Welcome.js
+++ webapp/src/Welcome.js
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import Box from "@material-ui/core/Box";
-import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
-import CircularProgress from "@material-ui/core/CircularProgress";
 import { makeStyles } from "@material-ui/core/styles";
 import { useSnackbar } from "notistack";
 import { EnterExitAnimation } from "./Animations";
+import { LoadingButton } from "./LoadingButton";
 import * as api from "./Api";
 
 const useStyles = makeStyles(theme => ({
@@ -20,16 +19,8 @@ const useStyles = makeStyles(theme => ({
     display: "flex",
     alignItems: "flex-end"
   },
-  enterContainer: {
-    position: "relative",
+  enterButton: {
     marginLeft: theme.spacing(2)
-  },
-  enterProgress: {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    marginTop: -12,
-    marginLeft: -12
   }
 }));
 
@@ -52,19 +43,16 @@ export function WelcomePanel({ onEnter, entering }) {
           onChange={e => setUsername(e.target.value)}
           onKeyPress={catchReturn}
         />
-        <Box className={classes.enterContainer}>
-          <Button
-            variant="contained"
-            color="primary"
-            disabled={!username || entering}
-            onClick={onEnter}
-          >
-            {t("Enter")}
-          </Button>
-          {entering && (
-            <CircularProgress size={24} className={classes.enterProgress} />
-          )}
-        </Box>
+        <LoadingButton
+          className={classes.enterButton}
+          variant="contained"
+          color="primary"
+          disabled={!username}
+          loading={entering}
+          onClick={onEnter}
+        >
+          {t("Enter")}
+        </LoadingButton>
       </Box>
     </Paper>
   );
