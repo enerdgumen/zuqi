d9d0028a897a8fbc6942d39efab1af0502dc1475
Mauro Rocchi <maurocchi@gmail.com>
2019-11-01 15:10:42 +0100

    add question answers component

--- webapp/src/Question.js
+++ webapp/src/Question.js
@@ -2,7 +2,14 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Checkbox from "@material-ui/core/Checkbox";
 import { makeStyles } from "@material-ui/core/styles";
+import { green, grey, red } from "@material-ui/core/colors";
 import { LoadingButton } from "./LoadingButton";
 
 const useStyles = makeStyles(theme => ({
@@ -11,10 +18,21 @@ const useStyles = makeStyles(theme => ({
   },
   challangeButton: {
     marginTop: theme.spacing(2)
+  },
+  greenCheckbox: {
+    color: green[600]
+  },
+  redCheckbox: {
+    color: red[700]
   }
 }));
 
-export function QuestionPanel({ question, onChallenge, disabled, challenging }) {
+export function QuestionPanel({
+  question,
+  onChallenge,
+  disabled,
+  challenging
+}) {
   const { t } = useTranslation();
   const classes = useStyles();
   return (
@@ -32,4 +50,41 @@ export function QuestionPanel({ question, onChallenge, disabled, challenging }) 
       </LoadingButton>
     </Paper>
   );
+}
+
+export function QuestionAnswers({ answers, onSelect, selection, status }) {
+  return (
+    <List component="nav">
+      {answers.map((answer, index) => {
+        const id = `answer-${index}`;
+        const isSelected = index === selection;
+        return (
+          <ListItem key={id} button onClick={() => onSelect(index)}>
+            <ListItemIcon>
+              <QuestionIcon status={isSelected && status} />
+            </ListItemIcon>
+            <ListItemText id={id} primary={answer} />
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+}
+
+function QuestionIcon({ status }) {
+  const { greenCheckbox, redCheckbox } = useStyles();
+  if (status === "loading") {
+    return <CircularProgress size={20} color={grey[900]} />;
+  }
+  if (status === "success" || status === "failure") {
+    return (
+      <Checkbox
+        className={status === "success" ? greenCheckbox : redCheckbox}
+        color="default"
+        edge="start"
+        checked={true}
+      />
+    );
+  }
+  return <Checkbox edge="start" checked={false} />;
 }
--- webapp/src/Question.stories.js
+++ webapp/src/Question.stories.js
@@ -1,8 +1,15 @@
 import React from "react";
 import centered from "@storybook/addon-centered/react";
 import { action } from "@storybook/addon-actions";
-import { withKnobs, boolean } from "@storybook/addon-knobs";
-import { QuestionPanel } from "./Question";
+import {
+  withKnobs,
+  boolean,
+  array,
+  number,
+  select
+} from "@storybook/addon-knobs";
+
+import { QuestionPanel, QuestionAnswers } from "./Question";
 
 export default {
   title: "Question",
@@ -11,9 +18,23 @@ export default {
 
 export const Panel = () => (
   <QuestionPanel
-    question="What was the first commercially available computer processor?"
+    question="What does the acronym CDN stand for in terms of networking?"
     onChallenge={action("Challenge")}
     challenging={boolean("Challenging", false)}
     disabled={boolean("Disabled", false)}
+  />
+);
+
+export const Answers = () => (
+  <QuestionAnswers
+    answers={array("Answers", [
+      "Content Delivery Network",
+      "Content Distribution Network",
+      "Computational Data Network",
+      "Compressed Data Network"
+    ])}
+    selection={number("Selection", 1)}
+    status={select("Status", ["loading", "success", "failure"], "success")}
+    onSelect={action("Selected")}
   />
 );
