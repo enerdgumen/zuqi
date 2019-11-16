b101a11270d2ccc74c6cef3c2d3de71944165944
Mauro Rocchi <maurocchi@gmail.com>
2019-11-16 18:16:46 +0100

    remove hardcoded hostname

--- webapp/src/Socket.js
+++ webapp/src/Socket.js
@@ -1,18 +1,18 @@
 export async function openSocket(username) {
   return new Promise((resolve, reject) => {
-    const socket = new WebSocket(
-      `ws://localhost:8000/?uid=${encodeURIComponent(username)}`
-    );
+    const hostname = window.location.hostname;
+    const url = `ws://${hostname}:8000/?uid=${encodeURIComponent(username)}`;
+    const socket = new WebSocket(url);
     socket.sendJson = data => {
       console.log("send", data);
-      setTimeout(() => socket.send(JSON.stringify(data)), 1000);
+      socket.send(JSON.stringify(data));
     };
     socket.onmessage = message => {
       const data = JSON.parse(message.data);
       console.log("message:", data);
       switch (data.event) {
         case "rejected":
-          return reject(Error('usernameNotAvailable'));
+          return reject(Error("usernameNotAvailable"));
         case "ready":
           return resolve(socket);
         default:
