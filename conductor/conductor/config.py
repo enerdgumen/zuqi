9e3e4a4faa7bd523e0b7c6f8dbfa354ca3e8b117
Mauro Rocchi <maurocchi@gmail.com>
2019-11-17 16:59:33 +0100

    remove prefix for env vars

--- conductor/conductor/config.py
+++ conductor/conductor/config.py
@@ -3,10 +3,9 @@ from environs import Env
 env = Env()
 env.read_env()
 
-with env.prefixed("CONDUCTOR_"):
-    port = env.int('PORT')
-    log_level = env('LOG_LEVEL')
-    challenge_timeout_seconds = env.int('CHALLENGE_TIMEOUT_SECONDS')
-    static_files_path = env('STATIC_FILES_PATH')
-    trivia_max_fetch_tentatives = env.int('TRIVIA_MAX_FETCH_TENTATIVES')
-    trivia_fetch_size = env.int('TRIVIA_FETCH_SIZE')
+port = env.int('PORT')
+log_level = env('LOG_LEVEL')
+challenge_timeout_seconds = env.int('CHALLENGE_TIMEOUT_SECONDS')
+static_files_path = env('STATIC_FILES_PATH')
+trivia_max_fetch_tentatives = env.int('TRIVIA_MAX_FETCH_TENTATIVES')
+trivia_fetch_size = env.int('TRIVIA_FETCH_SIZE')
