06eea4af20c9fb2fa855417fcb946a122f61790d
Mauro Rocchi <maurocchi@gmail.com>
2019-11-18 20:52:54 +0100

    cleanup dependences

--- webapp/src/Welcome.js
+++ webapp/src/Welcome.js
@@ -5,7 +5,6 @@ import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
-import { useSnackbar } from "notistack";
 import { LoadingButton } from "./LoadingButton";
 
 const useStyles = makeStyles(theme => ({
